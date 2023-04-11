79b431e20eed13dcd28fa325dc5129e56a02bfdd
IsraelEsparza <isra912747@gmail.com>
2023-04-10 23:29:01 -0700

    Services
    Service Get-Categories

---
+++ src/main/java/com/company/inventory/model/Category.java
@@ -0,0 +1,20 @@
+package com.company.inventory.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "category")
+@Data
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+
+}
