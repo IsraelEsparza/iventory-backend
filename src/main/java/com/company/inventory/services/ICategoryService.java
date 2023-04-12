f70f8ae32f8b7823ffc5ff82e055e8e2f47664de
IsraelEsparza <isra912747@gmail.com>
2023-04-11 21:23:12 -0700

    Services: Get-Categories-ById
    Implementation service Get-Categories-ById

--- src/main/java/com/company/inventory/services/ICategoryService.java
+++ src/main/java/com/company/inventory/services/ICategoryService.java
@@ -7,4 +7,5 @@ import org.springframework.http.ResponseEntity;
 public interface ICategoryService {
 
     public ResponseEntity<CategoryResponseRest> search();
+    public ResponseEntity<CategoryResponseRest> searchById(Long id);
 }
